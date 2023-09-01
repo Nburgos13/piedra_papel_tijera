e6c7a5fea15f390f4c45149466a6dc6c967053ee
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-08-31 22:37:29 -0300

    logica terminada - falta terminar de colocar aspectos mas interactivo como ser mensaje para mostrar las opciones que eligio la computadora

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,3 +1,34 @@
-print("hola mundo")
+import random
 
-print("hola mundo")
+# Piedra Papel y Tijera
+
+
+def jugar():
+    usuario = input("Escoge una opción: 'pi' para piedra, 'pa' para papel, 'ti' para tijera:\n").lower()
+    computadora = random.choice(["pi", "pa", "ti"])
+
+    if usuario == computadora:
+        return "¡ Empate !"
+
+    if gano_el_jugador(usuario, computadora):
+        return "¡ Ganaste !"
+
+    return "¡ Perdiste !"
+
+
+def gano_el_jugador(jugador, oponente):
+
+    # Retornar True si gana el jugador
+    # Piedra gana a tijera (pi > ti).
+    # Tijera gana a papel (ti > pa).
+    # Papel gana a piedra (pa > pi).
+    # Si ninguno es verdadero retornamos False el jugador perdio
+
+    if ((jugador == "pi" and oponente == "ti")
+        or (jugador == "ti" and oponente == "pa")
+        or (jugador == "pa" and oponente == "pi")):
+        return True
+    else:
+        return False
+
+print(jugar())
